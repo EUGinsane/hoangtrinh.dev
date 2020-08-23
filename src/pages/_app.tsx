5d9a5003f20c1b1d7454a5b430ad568d8defd949
Hoang <hoang@drobebox.com>
2020-08-23 15:41:17 +0700

    add eslint config for nextjs and fix all lint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,19 @@
-import '../styles/globals.scss';
 import Head from 'next/head';
+import { AppProps } from 'next/app';
+import '../styles/globals.scss';
 
-const _App = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Head>
-        <title>Hoang Trinh's Website</title>
-        <meta
-          name="description"
-          content="A passionated developer who loves solving problems."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-};
+const _App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>Hoang Trinh&apos;s Website</title>
+      <meta
+        name="description"
+        content="A passionated developer who loves solving problems."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
 export default _App;
